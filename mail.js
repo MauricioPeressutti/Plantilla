a8c4bc876fde2ded86d902673ad06efd330652ea
Mauricio <mauricioperessutti@gmail.com>
2023-03-31 18:29:03 -0300

    enviar mail a mp

--- mail.js
+++ mail.js
@@ -1,15 +1,25 @@
 function sendEmail() {
+    let email = document.getElementById("emailId").value;
+    let phone = document.getElementById("phoneId").value;
+    let name = document.getElementById("name").value;
+    let lastName = document.getElementById("lastName").value;
+    let description = document.getElementById("descripcionId").value;
+    debugger
     Email.send({
       Host: "smtp.elasticemail.com",
       Username: "enviador.boss@gmail.com",
       Password: "1C10ECBFC18E8BDE84054FA5995A0966EAE8",
-      To: 'enviador.boss@gmail.com',
-      From: "enviador.boss@gmail.com",
-      Subject: "TESTING TO MAIL",
-      Body: "Well that was easy TESTING to mail!!",
+      To: 'mauricioperessutti@gmail.com',
+      From: email,
+      Subject: "Envio de mail desde la web de IGRIEGA de " + name + " " + lastName + "Telefono: " + phone,
+      Body: description,
     })
       .then(function (message) {
         alert("Mail has been sent successfully")
       });
+
+}
+
+function checkValues(){
 
 }
